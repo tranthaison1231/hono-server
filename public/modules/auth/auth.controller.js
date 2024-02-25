95b698ef6ed35a6d8581b174464caaf46fc29976
son.tran <son.tran@team.enouvo.com>
2024-02-25 21:31:53 +0700

    feat(master): update repo

--- public/modules/auth/auth.controller.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.router = void 0;
-const hono_1 = require("hono");
-exports.router = new hono_1.Hono();
-exports.router.get("/sign-in", async (c) => {
-    return c.json({ hello: "world" }, 200);
-});
-//# sourceMappingURL=auth.controller.js.map
